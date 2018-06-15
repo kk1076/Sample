cbdc20c5d44ac3077ed416628443a0250cb51778
kk1076 <karthikeyan.d.k@capgemini.com>
2018-06-15 09:37:41 +0530

    Updated Test Env Path

--- build/unicorn-sync.ps1
+++ build/unicorn-sync.ps1
@@ -2,12 +2,12 @@ $ErrorActionPreference = 'Stop'
 
 # This is an example PowerShell script that will remotely execute a Unicorn sync using the new CHAP authentication system.
 
-$ScriptPath = 'C:\Users\kk43\Documents\Visual Studio 2015\Projects\SampleWebSite\build\unicorn.psm1'
+$ScriptPath = 'C:\Git Code\Sample\build\unicorn.psm1'
 
 Import-Module $ScriptPath
 
 # SYNC ALL CONFIGURATIONS
-Sync-Unicorn -ControlPanelUrl 'http://devwebsite/unicorn.aspx' -SharedSecret 'sBPcp7j95ALLtMKFAn25nC3Sh76GPS4fDyNdUwvXfsxApcWA2LpfbJX7C3Cgv22qaphDdn2XhzVv7uMBWZWd97SU4X9gyc4p2dHC'
+Sync-Unicorn -ControlPanelUrl 'http://testenv/unicorn.aspx' -SharedSecret 'sBPcp7j95ALLtMKFAn25nC3Sh76GPS4fDyNdUwvXfsxApcWA2LpfbJX7C3Cgv22qaphDdn2XhzVv7uMBWZWd97SU4X9gyc4p2dHC'
 
 # Note: you may pass -Verb 'Reserialize' for remote reserialize. Usually not needed though.
 
