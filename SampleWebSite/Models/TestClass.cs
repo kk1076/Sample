9f1ec01ac15ab41bc6ab363457f155cacd83328b
kk1076 <karthikeyan.d.k@capgemini.com>
2018-06-21 12:21:16 +0530

    unit test added

---
+++ SampleWebSite/Models/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleWebSite.Models
+{
+    public class TestClass
+    {
+        int s;
+
+        [Test]
+        public void Addmethod()
+        {
+            int x = 10;
+            int y = 20;
+            s = x + y;
+        }
+
+        [Test]
+        public void Submethod()
+        {
+            int p = s - 10;
+            Assert.AreEqual(p, 20);
+        }
+
+    }
+}
