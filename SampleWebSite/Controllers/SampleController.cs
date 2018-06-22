e08560cbb7f58ccd42d6ed69fe82dbe99c8534ce
kk1076 <karthikeyan.d.k@capgemini.com>
2018-06-22 18:40:02 +0530

    test added

--- SampleWebSite/Controllers/SampleController.cs
+++ SampleWebSite/Controllers/SampleController.cs
@@ -20,6 +20,10 @@ namespace SampleWebSite.Controllers
 
             return View(model);
         }
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
     }
 }
 
